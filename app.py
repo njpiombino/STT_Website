94a234373e4058d135b53ed50668264475e879d0
Nicholas Piombino <njpiombino@wm.edu>
2026-01-12 16:09:31 -0500

    Template from YT

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+# Imports
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
+# My App
 app = Flask(__name__)
+Scss(app)
 
-
+# Routes to Webpages
 @app.route("/")
 def index():
-    return "Testing 123"
+    return render_template("index.html")
 
 
+# Runner and Debugger
 if __name__ == "__main__":
     app.run(debug=True)
